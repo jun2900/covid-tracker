25940dc60a14f8dc76590218a743a8ffc59187b3
jun2900 <jeremiahmaramis00@gmail.com>
2020-05-01 09:44:23 +0800

    inline styling

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -17,7 +17,7 @@ const CountryPicker = ({ handleCountryChange }) => {
     <div className={styles.container}>
       <Form>
         <Label>
-          <h2>Pilih Negara:</h2>
+          <h5 style={{ display: "inline" }}>Pilih Negara:</h5>
           <select onChange={(e) => handleCountryChange(e.target.value)}>
             <option value="global" disabled>
               Select Country
